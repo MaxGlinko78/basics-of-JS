d633541f968f08e04eda757cd795533316e07e35
Glinko <example.gmail.com>
2020-12-29 21:22:36 +0300

    first commit

---
+++ js/main.js
@@ -0,0 +1,35 @@
+// "use strict";
+
+// const obj = {
+//     name :"Glinko",
+//     age: 105,
+//     isMarried: true,
+// };
+
+// // console.log(obj);
+
+
+// // const result = confirm("Are you Here?");
+
+// // console.log(result); 
+
+// const  answer = prompt("Are you 18?","18");
+
+// console.log(typeof(answer)); 
+
+// const answers =[];
+// answers[0] = prompt("Ваше Имя?",'');
+// answers[1] = prompt("Ваша Фамилия?","");
+// answers[2] = prompt("Ваш Возраст?","");
+// answers[3] = prompt("Ваш E-Mail?","");
+// answers[4] = prompt("Ваш Телефон","");
+
+// document.write(answers);
+
+let incr = 10,
+    decr = 10;
+
+    incr++;
+    decr--;
+
+    console.log(incr , decr);
