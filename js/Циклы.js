bd1ebcdefa09d547273d8e9786e3f02fbbc03dae
Glinko <example.gmail.com>
2020-12-30 21:29:05 +0300

    start layout

---
+++ js/Циклы.js
@@ -0,0 +1,77 @@
+ "use strict";
+
+// const obj = {
+//     name :"Glinko",
+//     age: 105,
+//     isMarried: true,
+// };
+
+// // console.log(obj);
+
+
+// // const result = confirm("Are you Here?");
+
+// // console.log(result); 
+
+// const  answer = prompt("Are you 18?","18");
+
+// console.log(typeof(answer)); 
+
+// const answers =[];
+// answers[0] = prompt("Ваше Имя?",'');
+// answers[1] = prompt("Ваша Фамилия?","");
+// answers[2] = prompt("Ваш Возраст?","");
+// answers[3] = prompt("Ваш E-Mail?","");
+// answers[4] = prompt("Ваш Телефон","");
+
+// document.write(answers);
+
+// let incr = 10,
+//     decr = 10;
+
+//     incr++;
+//     decr--;
+
+//     console.log(incr , decr);
+
+// const numberOfFilms = +prompt("Сколько Фильмов вы уже псмотрели",'');
+
+
+// const personalMoviesDB = {
+//     count: numberOfFilms,
+//     movies:{}, 
+//     actors:{},
+//     genres:[],
+//     privat: false
+
+// };
+
+// const a = prompt("Один из последних просмотренных фильмов?",''),
+//       b = prompt("насколько оцениет его?",''),
+//       c = prompt("Один из последних просмотренных фильмов?",''),
+//       d = prompt("насколько оцениет его?",'');
+
+//       personalMoviesDB.movies[a]=b; 
+//       personalMoviesDB.movies[c]=d; 
+
+//     console.log(personalMoviesDB);
+ 
+
+
+// let num = 50;
+// while(num<=100){
+//     console.log(num);
+//     num++;
+// }
+
+// do{console.log(num);
+//     num++;
+
+// }while(num<=100);
+
+for(let i=1; i<18; i++ ){
+    if (i === 16){
+        break;
+    }
+    console.log(i);
+};
