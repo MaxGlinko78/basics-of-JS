cee2098a16aa96a6853920913aa16734f1a7522b
Glinko <example.gmail.com>
2021-01-11 21:03:31 +0300

    HW3

---
+++ js/lesson17.js
@@ -0,0 +1,28 @@
+"use strict";
+
+const str = "test";
+const arr = [1, 2, 3,];
+
+console.log(str[3]);
+
+console.log(str.toLocaleUpperCase());
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+
+const logg = "Hello world";
+
+console.log(logg.slice(6,11));
+
+console.log(logg.substring(2,11));
+console.log(logg.substr (1,8));
+
+
+const num = 12.2;
+
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
